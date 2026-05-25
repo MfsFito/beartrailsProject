a7fa588da86c1331201612bf24c76c098df8b00a
MfsFito <fitorzk12345@gmail.com>
2026-05-25 17:57:20 +0800

    nambah model destinasionimage

---
+++ app/Models/DestinationImage.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DestinationImage extends Model
+{
+    protected $fillable = ['destination_id', 'image', 'order'];
+
+    public function destination()
+    {
+        return $this->belongsTo(Destination::class);
+    }
+}
