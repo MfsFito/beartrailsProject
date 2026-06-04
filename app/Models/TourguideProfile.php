2053cea2ac7ff2d751314125a9e703cfbee87d2f
MfsFito <fitorzk12345@gmail.com>
2026-06-05 07:28:45 +0800

    nambah relasi di portofolio tourguide

--- app/Models/TourguideProfile.php
+++ app/Models/TourguideProfile.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TourguideProfile extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'user_id',
         'phone',
@@ -20,8 +17,21 @@ class TourguideProfile extends Model
         'rating',
     ];
 
+    // Relasi ke user
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    // Relasi ke availabilities
+    public function availabilities()
+    {
+        return $this->hasMany(TourguideAvailability::class);
+    }
+
+    // Relasi ke portfolios
+    public function portfolios()
+    {
+        return $this->hasMany(TourguidePortfolio::class);
     }
 }
